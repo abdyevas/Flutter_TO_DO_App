6360dc409d18f1ed7ce03e84ef6714c04eca10a0
abdyevas <sabina.abdyeva@ufaz.az>
2023-12-07 23:26:30 +0400

    add theme

--- to_do_app/lib/main.dart
+++ to_do_app/lib/main.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 101, 51, 132),);
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 41, 4, 64),
+);
+
 void main() {
   runApp(
-    const MaterialApp(
-      
+    MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorScheme,
+      ),
+      theme: ThemeData().copyWith(
+        useMaterial3: true, 
+        colorScheme: kColorScheme,
+      ),
     ),
   );
 }
